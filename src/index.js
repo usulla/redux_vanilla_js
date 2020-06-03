56842589fbd087aebc5562bf1adffbebe6e956af
Сухарева Мария <maria_suxareva@mail.ru>
2020-06-03 17:29:31 +0300

    Finish

--- src/index.js
+++ src/index.js
@@ -1,1 +1,65 @@
 import './styles.css'
+import { createStore, applyMiddleware } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import { rootReducer } from './redux/rootReducer'
+import { increment, decrement, asyncIncrement, changeTheme } from './redux/actions'
+import thunk from 'redux-thunk';
+import logger from 'redux-logger'
+
+const counter = document.getElementById('counter')
+const addBtn = document.getElementById('add')
+const subBtn = document.getElementById('sub')
+const asyncBtn = document.getElementById('async')
+const themeBtn = document.getElementById('theme')
+
+// function logger(state) {
+//     return function (next) {
+//         return function (action) {
+//             console.log('state', state.getState())
+//             console.log('action', action)
+//             return next(action)
+//         }
+//     }
+// }
+
+// const store = createStore(
+//     rootReducer,
+//     compose(
+//         applyMiddleware(thunk, logger),
+//         window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+//     )
+// )
+const store = createStore(
+    rootReducer,
+    composeWithDevTools(
+        applyMiddleware(thunk, logger)
+    )
+)
+
+addBtn.addEventListener('click', () => {
+    store.dispatch(increment())
+})
+
+subBtn.addEventListener('click', () => {
+    store.dispatch(decrement())
+})
+
+asyncBtn.addEventListener('click', () => {
+    store.dispatch(asyncIncrement())
+})
+
+themeBtn.addEventListener('click', () => {
+    const newTheme = document.body.classList.contains('light')
+        ? 'dark' :
+        'light'
+    store.dispatch(changeTheme(newTheme))
+})
+
+store.subscribe(() => {
+    const state = store.getState()
+    counter.textContent = state.counter
+    document.body.className = state.theme.value;
+    [addBtn, subBtn, themeBtn, asyncBtn].forEach(btn => btn.disabled = state.theme.disabled)
+})
+
+store.dispatch({ type: 'INIT_APPLICATION' })
